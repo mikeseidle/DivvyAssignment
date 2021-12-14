79e45e77dece7a4a53b603e168f68b8a50a10d6c
mikeseidle <seidle.8@osu.edu>
2021-12-13 22:42:11 -0500

    Basic Navigation and Props Passing

--- screens/BusinessDetail.js
+++ screens/BusinessDetail.js
@@ -1,8 +1,42 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import {View, Text, NativeModules} from 'react-native';
+
+const {BarChart} = NativeModules;
+
+console.log("Native",NativeModules)
 
 export default class BusinessDetail extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      item: this.props.route.params.item
+    }
+  }
   render() {
-    return <View />;
+    //console.log('Our Company', this.props.route.params.item)
+    return (
+      <View style={{flex: 1}}>
+        <View style={{flex: 2, borderWidth: 1, justifyContent:'flex-end', backgroundColor:'gray'}}>
+           <View style= {{padding: 5, borderWidth: 1}}>
+
+           <Text style={{fontSize: 24, }}>{this.state.item.name}</Text>
+           </View>
+        </View>
+        <View style={{flex: 6, borderWidth: 1,  }}>
+        <View style= {{padding: 5}}>
+          <Text style= {{opacity: 0.7}}>
+          Location
+          </Text>
+          <Text style= {{fontSize: 16}}>
+            {this.state.item.location.address} {this.state.item.location.city}, {this.state.item.location.country}
+          </Text>
+          <Text style= {{fontSize: 16}}>
+            {this.state.item.location.city}, {this.state.item.location.country}
+          </Text>
+        </View>
+        </View>
+       
+      </View>
+    );
   }
 }
--- screens/Businesses.js
+++ screens/Businesses.js
@@ -1,13 +1,37 @@
 import * as React from 'react';
-import {FlatList, Text, View} from 'react-native';
+import {FlatList, Text, View, TouchableOpacity} from 'react-native';
 
-const DATA = [];
 
-const CompanyItem = ({title}) => {
+const CompanyItem = ({item, nav}) => {
   return (
-    <View style={{width: '90%', height: 60, backgroundColor: 'gray'}}>
-      <Text>Test: {title}</Text>
-    </View>
+    <TouchableOpacity
+      onPress={() => {
+        console.log('from item',item)
+
+        nav.navigate('Profile',{item:item, name: item.name})
+      }}
+      style={{
+        width: '90%',
+        height: 60,
+        backgroundColor: 'gray',
+        alignSelf: 'center',
+        marginVertical: 3,
+        borderRadius: 5,
+        padding: 5,
+      }}>
+      <View style={{flexDirection: 'row', flex: 1}}>
+        <View style={{flex: 1, borderWidth: 1}}></View>
+        <View style={{flex: 6, borderWidth: 1, justifyContent: 'center'}}>
+          <Text style={{color: 'white', fontSize: 20}}>
+            {item.name}, {item.location.city}
+          </Text>
+        </View>
+        <View style={{flex: 1, borderWidth: 1}}>
+          
+
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 };
 
@@ -17,26 +41,154 @@ export default class Businesses extends React.Component {
     super(props);
     this.state = {
       data: [
-        {id: '1', title: 'num 1'},
-        {id: '2', title: 'num 2'},
+        {
+          id: 1,
+          name: 'Meedoo',
+          location: {
+            address: '20 Buena Vista Parkway',
+            city: 'Austin',
+            country: 'United States',
+          },
+          revenue: [
+            {
+              seq: 0,
+              date: '2019-03-12 00:14:10',
+              value: 77793740.07,
+            },
+            {
+              seq: 1,
+              date: '2019-02-12 00:14:10',
+              value: 17659305.4,
+            },
+            {
+              seq: 2,
+              date: '2019-01-12 00:14:10',
+              value: 79457805.93,
+            },
+            {
+              seq: 3,
+              date: '2018-12-12 00:14:10',
+              value: 12291141.73,
+            },
+            {
+              seq: 4,
+              date: '2018-11-12 00:14:10',
+              value: 15418844.3,
+            },
+            {
+              seq: 5,
+              date: '2018-10-12 00:14:10',
+              value: 73533303.23,
+            },
+          ],
+        },
+        {
+          id: 2,
+          name: 'Linkbuzz',
+          location: {
+            address: '8 Burning Wood Center',
+            city: 'Miami',
+            country: 'United States',
+          },
+          revenue: [
+            {
+              seq: 0,
+              date: '2019-03-12 00:14:10',
+              value: 13676205.39,
+            },
+            {
+              seq: 1,
+              date: '2019-02-12 00:14:10',
+              value: 51026136.95,
+            },
+            {
+              seq: 2,
+              date: '2019-01-12 00:14:10',
+              value: 30522302.47,
+            },
+            {
+              seq: 3,
+              date: '2018-12-12 00:14:10',
+              value: 58908776.81,
+            },
+            {
+              seq: 4,
+              date: '2018-11-12 00:14:10',
+              value: 30023910.49,
+            },
+            {
+              seq: 5,
+              date: '2018-10-12 00:14:10',
+              value: 34391012.83,
+            },
+          ],
+        },
+        {
+          id: 3,
+          name: 'Plajo',
+          location: {
+            address: '06 Ludington Trail',
+            city: 'Danbury',
+            country: 'United States',
+          },
+          revenue: [
+            {
+              seq: 0,
+              date: '2019-03-12 00:14:10',
+              value: 4491055.0,
+            },
+            {
+              seq: 1,
+              date: '2019-02-12 00:14:10',
+              value: 72885494.72,
+            },
+            {
+              seq: 2,
+              date: '2019-01-12 00:14:10',
+              value: 37467126.29,
+            },
+            {
+              seq: 3,
+              date: '2018-12-12 00:14:10',
+              value: 44817850.33,
+            },
+            {
+              seq: 4,
+              date: '2018-11-12 00:14:10',
+              value: 89470533.7,
+            },
+            {
+              seq: 5,
+              date: '2018-10-12 00:14:10',
+              value: 9481381.0,
+            },
+          ],
+        },
       ],
       refreshing: true,
     };
   }
 
-  renderCompany = (item) => 
-    <CompanyItem title={item.title} />;
-  
+  //renderCompany = (data) => <CompanyItem item={data.item} nav={data.handleClick} />;
+  handleClick(item){
+    console.log('test')
+    console.log(item)
+    this.props.navigation.navigate('Profile', {
+      item: item
+    })
+  }
   render() {
     return (
       <View style={{flex: 1, backgroundColor: '#FFFFFF'}}>
-        <Text>Foo bar shiby</Text>
-        <CompanyItem title={'number one'} />
-        <FlatList
-          data={this.state.data}
-          renderItem={item => this.renderCompany(item)}
-          keyExtractor={item => item.id}
-        />
+        
+        <View style={{flex: 7, borderWidth: 1, borderColor: 'green'}}>
+          <FlatList
+            data={this.state.data}
+            //renderItem={(item) => this.renderCompany(item, this.handleClick())}
+            renderItem={(item) => <CompanyItem item={item.item} nav={this.props.navigation} />}
+            keyExtractor={(item) => item.id.toString()}
+          />
+        </View>
       </View>
     );
 
